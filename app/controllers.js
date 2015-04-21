1a1c98fa71e6f43691200e07316d60dd05a1968b
Janak Mayer <janak.mayer@gmail.com>
2015-04-21 16:26:24 -0400

    Proof of concept with sigmajs working

--- app/controllers.js
+++ app/controllers.js
@@ -1,36 +1,83 @@
 (function() {
 
     var vizController = function ($scope, SyriaTwitterAPI) {
-        $scope.params = {};
-        $scope.params.availableGroups = []
+
+        $scope.giraph = {
+        "nodes": [
+        {
+          "id": "n0",
+          "label": "A node",
+          "x": 0,
+          "y": 0,
+          "size": 3
+        },
+        {
+          "id": "n1",
+          "label": "Another node",
+          "x": 3,
+          "y": 1,
+          "size": 2
+        },
+        {
+          "id": "n2",
+          "label": "And a last one",
+          "x": 1,
+          "y": 3,
+          "size": 1
+        }
+        ],
+        "edges": [
+        {
+          "id": "e0",
+          "source": "n0",
+          "target": "n1"
+        },
+        {
+          "id": "e1",
+          "source": "n1",
+          "target": "n2"
+        },
+        {
+          "id": "e2",
+          "source": "n2",
+          "target": "n0"
+        }
+        ]
+        };
+
+
+
+    //    $scope.params = {};
+    //    $scope.params.availableGroups = []
         $scope.getData = function () {
 
-            SyriaTwitterAPI.graphGroups({
+            //SyriaTwitterAPI.graphGroups({
+            //}).success(function(response){
+            //
+            //    $scope.params.availableGroups = response.groups;
+            //    $scope.params.groups = response.groups;
+
+            SyriaTwitterAPI.graphData({
+                "clusters": "to come later"
             }).success(function(response){
+                console.log(response);
+                $scope.graph = response;
 
-                $scope.params.availableGroups = response.groups;
-                $scope.params.groups = response.groups;
-
-                SyriaTwitterAPI.graphData({
-                    "clusters": $scope.params.groups
-                }).success(function(response){
-
-                    $scope.data = response.data;
-
-                });
             });
+            //});
         };
         $scope.getData();
-
-        $scope.updateData = function () {
-            SyriaTwitterAPI.graphData({
-                "clusters": $scope.params.groups
-            }).success(function(response){
-
-                $scope.data = response.data;
-
-            });
-        }
+    //
+    //    $scope.updateData = function () {
+    //        SyriaTwitterAPI.graphData({
+    //            "clusters": $scope.params.groups
+    //        }).success(function(response){
+    //
+    //            $scope.data = response.data;
+    //
+    //        });
+    //    }
+    //
 
     };
 
--- app/directives.js
+++ app/directives.js
@@ -1,101 +1,88 @@
 (function() {
 
+	'use strict';
 
-    var userGraph = function(){
-        function link(scope, element, attributes){
+    var sigmaGraph = function() {
 
-            //Constants for the SVG
-            var width = 500,
-                height = 500;
+		//over-engineered random id, so that multiple instances can be put on a single page
+		var divId = 'sigmjs-dir-container-'+Math.floor((Math.random() * 999999999999))+'-'+Math.floor((Math.random() * 999999999999))+'-'+Math.floor((Math.random() * 999999999999));
 
-            //Set up the colour scale
-            var color = d3.scale.category20();
+        function link(scope, element, attrs) {
 
+            // Let's first initialize sigma:
+            var s = new sigma({
+                container: divId,
+                settings: {
+                    defaultNodeColor: '#ec5148',
+                    scalingMode: 'outside',
+                    maxNodeSize:3,
+                    minEdgeSize: 0.1,
+                    maxEdgeSize: 0.2,
+                    zoomMax: 10,
+                    zoomMin: .01
 
-
-            //Append a SVG to the body of the html page. Assign this SVG as an object to svg
-
-
-            //Create the graph data structure out of the data object on the scope, once it is assigned by the controller
-            scope.$watch('data', function(graph) {
-                if (typeof graph !== "undefined") {
-
-                d3.select("#userGraph").remove();
-                var svg = d3.select(element[0]).append("svg")
-                    .attr("id", "userGraph")
-                    .attr("width", width)
-                    .attr("height", height);
-
-                    //Set up the force layout
-                    var force = d3.layout.force()
-                        .charge(-120)
-                        .linkDistance(30)
-                        .size([width, height]);
-
-                    force.nodes(graph.nodes)
-                        .links(graph.links)
-                        .start();
-
-                    //Create all the line svgs but without locations yet
-                    var link = svg.selectAll(".link")
-                        .data(graph.links)
-                        .enter()
-                        .append("line")
-                        .attr("class", "link")
-                        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-                    //Do the same with the circles for the nodes
-                    var node = svg.selectAll(".node")
-                        .data(graph.nodes)
-                        .enter().append("circle")
-                        .attr("class", "node")
-                        .attr("r", 5)
-                        .style("fill", function(d) { return color(d.group); })
-                        .call(force.drag);
-
-                    node.append("title")
-                        .text(function(d) { return d.name; });
-
-
-                    //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code
-                    // is using to update the attributes of the SVG elements
-                    force.on("tick", function() {
-                        link.attr("x1", function(d) { return d.source.x; })
-                            .attr("y1", function(d) { return d.source.y; })
-                            .attr("x2", function(d) { return d.target.x; })
-                            .attr("y2", function(d) { return d.target.y; });
-
-                        node.attr("cx", function(d) { return d.x; })
-                            .attr("cy", function(d) { return d.y; });
-                      });
                 }
             });
 
+            s.bind('clickNode', function(e) {
+                console.log('node ' + e.data.node.id + ' was clicked')
+            });
 
+            scope.$watch('graph', function(newVal,oldVal) {
+                if (1==1) {
+                    console.log('graph watch was triggered');
+                    s.graph.clear();
+                    s.graph.read(scope.graph);
+                    s.refresh();
+                    //if(scope.releativeSizeNode) {
+                    //    //this feature needs the plugin to be added
+                    //    sigma.plugins.relativeSize(s, 2);
+                    //    console.log('relative size was triggered');
+                    //
+                    //}
+                }
+            });
 
+            scope.$watch('width', function(newVal,oldVal) {
+                console.log("graph width: "+scope.width);
+                element.children().css("width",scope.width);
+                s.refresh();
+                window.dispatchEvent(new Event('resize')); //hack so that it will be shown instantly
+            });
+            scope.$watch('height', function(newVal,oldVal) {
+                console.log("graph height: "+scope.height);
+                element.children().css("height",scope.height);
+                s.refresh();
+                window.dispatchEvent(new Event('resize'));//hack so that it will be shown instantly
+            });
+
+            element.on('$destroy', function() {
+                s.graph.clear();
+            });
         }
+
         return {
-            link: link,
+			link: link,
             restrict: 'E',
-            scope: {
-                data: '=',
-                v: '@'
-            }
-        };
-    };
+			template: '<div id="'+divId+'" style="width: 100%;height: 100%;"></div>',
+			scope: {
+				//@ reads the attribute value, = provides two-way binding, & works with functions
+				graph: '=',
+				width: '@',
+				height: '@',
+				releativeSizeNode: '='
+			}
 
+		};
 
-
-
-
+	};
 
     /**
      *
      * Pass all functions into module
      */
-
     angular
         .module('myApp')
-        .directive('userGraph', userGraph)
+        .directive('sigmaGraph', sigmaGraph)
 
-}());
+})();
