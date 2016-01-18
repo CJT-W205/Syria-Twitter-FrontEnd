2486d5e8bb0d35fd13eaebedcfe5704e0436000b
Janak Mayer <janak.mayer@gmail.com>
2016-01-18 12:57:45 -0500

    Updating to use data.enalytica.com for the server

--- app/services.js
+++ app/services.js
@@ -1,7 +1,7 @@
 (function() {
     var SyriaTwitterAPI = function($http) {
 
-        var servers = {'local': 'http://127.0.0.1:5000', 'production': 'http://169.53.140.164:8000'};
+        var servers = {'local': 'http://127.0.0.1:5000', 'production': 'http://data.enalytica.com:9600'};
         var server_url = servers.production;
 
         var factory = {};
@@ -9,7 +9,6 @@
             return $http.get(server_url+'/graph');
         };
         factory.graphData = function(params) {
-            console.log(params)
             return $http.put(server_url+'/graph', params);
         };
         factory.userDetails = function(params) {
